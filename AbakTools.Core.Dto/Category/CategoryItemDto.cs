bdef2f0369d248f45e6f174d83fd0020fcc54df8
Wojciech Ogorzalek <wojciech.ogorzalek@gmail.com>
2020-05-24 22:20:33 +0200

    Add AbakTools.Core.Dto

---
+++ AbakTools.Core.Dto/Category/CategoryItemDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace AbakTools.Core.Dto.Category
+{
+    public class CategoryItemDto
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("displayorder")]
+        public int DisplayOrder { get; set; }
+
+        [JsonPropertyName("active")]
+        public bool Active { get; set; }
+
+        [JsonPropertyName("leveldepth")]
+        public byte? LevelDepth { get; set; }
+    }
+}
---
+++ AbakTools.Core.Dto/Category/GetCategoryResponseDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbakTools.Core.Dto.Category
+{
+    public class GetCategoryResponseDto
+    {
+        public int? ParentId { get; set; }
+        public string ParentName { get; set; }
+        public byte? LevelDepth { get; set; }
+        public bool? Active { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string LinkRewrite { get; set; }
+        public string MetaTitle { get; set; }
+        public string MetaDescription { get; set; }
+        public string MetaKeywords { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
