ef0b725ea4855b0097246dc091393bdc07e83de7
Wojciech Ogorzalek <wojciech.ogorzalek@gmail.com>
2020-09-11 15:45:59 +0200

    Fix product synctonization

--- AbakTools.Core.Infrastructure/PrestaShop/PrestaShopSynchronizeService.Product.cs
+++ AbakTools.Core.Infrastructure/PrestaShop/PrestaShopSynchronizeService.Product.cs
@@ -69,7 +69,7 @@ namespace AbakTools.Core.Infrastructure.PrestaShop
                     product = productRepository.Get(product.Id);
                     Bukimedia.PrestaSharp.Entities.product psProduct = GetPsProduct(product.WebId);
 
-                    if(psProduct == null && product.WebId.HasValue)
+                    if (psProduct == null && product.WebId.HasValue)
                     {
                         product.WebId = null;
                     }
@@ -89,78 +89,78 @@ namespace AbakTools.Core.Infrastructure.PrestaShop
                             psProduct = new Bukimedia.PrestaSharp.Entities.product();
                         }
 
-                        psProduct.id_tax_rules_group = GetTaxRuleGroupId(product.Tax);
-                        psProduct.price = product.Price;
-
-                        var name = Functions.GetPrestaShopName(product.Name);
-
-                        prestaShopClient.SetLangValue(psProduct, x => x.name, name);
-                        prestaShopClient.SetLangValue(psProduct, x => x.link_rewrite, Functions.GetLinkRewrite(name));
-                        prestaShopClient.SetLangValue(psProduct, x => x.description_short, Functions.GetPrestaShopDescriptionShort(product.DescriptionShort));
-                        prestaShopClient.SetLangValue(psProduct, x => x.description, product.Description);
-
-                        product = productRepository.Get(product.Id);
-
-                        foreach (var category in product.Categories)
+                        if (psProduct != null)
                         {
-                            if (category.WebId.HasValue && !category.IsDeleted && category.Synchronize != Framework.SynchronizeType.Deleted)
+
+                            psProduct.id_tax_rules_group = GetTaxRuleGroupId(product.Tax);
+                            psProduct.price = product.Price;
+
+                            var name = Functions.GetPrestaShopName(product.Name);
+
+                            prestaShopClient.SetLangValue(psProduct, x => x.name, name);
+                            prestaShopClient.SetLangValue(psProduct, x => x.link_rewrite, Functions.GetLinkRewrite(name));
+                            prestaShopClient.SetLangValue(psProduct, x => x.description_short, Functions.GetPrestaShopDescriptionShort(product.DescriptionShort));
+                            prestaShopClient.SetLangValue(psProduct, x => x.description, product.Description);
+
+                            foreach (var category in product.Categories)
                             {
-                                try
+                                if (category.WebId.HasValue && !category.IsDeleted && category.Synchronize != Framework.SynchronizeType.Deleted)
                                 {
-                                    var psCategory = prestaShopClient.CategoryFactory.Get(category.WebId.Value);
-                                    if (psCategory != null)
+                                    try
                                     {
-                                        psProduct.associations.categories.Add(new Bukimedia.PrestaSharp.Entities.AuxEntities.category(psCategory.id.Value));
+                                        var psCategory = prestaShopClient.CategoryFactory.Get(category.WebId.Value);
+                                        if (psCategory != null)
+                                        {
+                                            psProduct.associations.categories.Add(new Bukimedia.PrestaSharp.Entities.AuxEntities.category(psCategory.id.Value));
+                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        logger.LogError(ex.ToString());
                                     }
                                 }
-                                catch (Exception ex)
+                            }
+
+                            psProduct.active = (product.Active && !product.IsDeleted) ? 1 : 0;
+                            psProduct.state = 1;
+                            psProduct.available_for_order = 1;
+                            psProduct.position_in_category = 0;
+                            psProduct.minimal_quantity = product.MinimumOrderQuantity > 0 ? product.MinimumOrderQuantity : 1;
+                            psProduct.id_category_default = product.Categories.Where(x => x.WebId.HasValue && !x.IsDeleted &&
+                                x.Synchronize != Framework.SynchronizeType.Deleted).Max(x => x.WebId);
+
+                            psProduct = SaveOrUpdatePsProduct(psProduct, product);
+                        }
+                    }
+
+                    try
+                    {
+
+                        if (psProduct != null)
+                        {
+                            foreach (var image in product.Images)
+                            {
+                                SynchronizeImage(product, psProduct, image);
+                            }
+
+                            if (psProduct.associations.images.Count != product.Images.Count)
+                            {
+                                foreach (var psImage in psProduct.associations.images)
                                 {
-                                    logger.LogError(ex.ToString());
+                                    if (!product.Images.Any(x => x.WebId.HasValue && x.WebId == psImage.id))
+                                    {
+                                        prestaShopClient.ImageFactory.DeleteProductImage(psProduct.id.Value, psImage.id);
+                                    }
                                 }
                             }
+
+                            SaveOrUpdatePsProduct(psProduct, product);
                         }
 
-                        psProduct.active = (product.Active && !product.IsDeleted) ? 1 : 0;
-                        psProduct.state = 1;
-                        psProduct.available_for_order = 1;
-                        psProduct.position_in_category = 0;
-                        psProduct.minimal_quantity = product.MinimumOrderQuantity > 0 ? product.MinimumOrderQuantity : 1;
-                        psProduct.id_category_default = product.Categories.Where(x => x.WebId.HasValue && !x.IsDeleted &&
-                            x.Synchronize != Framework.SynchronizeType.Deleted).Max(x => x.WebId);
                     }
-
-                    if (psProduct != null)
+                    catch (Exception ex)
                     {
-
-                        if (psProduct.id.HasValue && psProduct.id.Value > 0)
-                        {
-                            logger.LogInformation($"Update product id: {product.Id}, name: {product.Name}");
-                            prestaShopClient.ProductFactory.Update(psProduct);
-                        }
-                        else
-                        {
-                            logger.LogInformation($"Add new product id: {product.Id}, name: {product.Name}");
-                            psProduct = prestaShopClient.ProductFactory.Add(psProduct);
-                        }
-
-                        product.WebId = (int)psProduct.id;
-                    }
-
-
-                    foreach (var image in product.Images)
-                    {
-                        SynchronizeImage(product, psProduct, image);
-                    }
-
-                    if (psProduct.associations.images.Count != product.Images.Count)
-                    {
-                        foreach (var psImage in psProduct.associations.images)
-                        {
-                            if (!product.Images.Any(x => x.WebId.HasValue && x.WebId == psImage.id))
-                            {
-                                prestaShopClient.ImageFactory.DeleteProductImage(psProduct.id.Value, psImage.id);
-                            }
-                        }
+                        logger.LogError($"Synchronize product Id: {product.Id} error.{Environment.NewLine}{ex}");
                     }
 
                     if (product.Synchronize == Framework.SynchronizeType.Deleted)
@@ -243,5 +243,22 @@ namespace AbakTools.Core.Infrastructure.PrestaShop
             return tax.WebId;
         }
 
+        private Bukimedia.PrestaSharp.Entities.product SaveOrUpdatePsProduct(Bukimedia.PrestaSharp.Entities.product psProduct, ProductEntity product)
+        {
+            if (psProduct.id.HasValue && psProduct.id.Value > 0)
+            {
+                logger.LogInformation($"Update product id: {product.Id}, name: {product.Name}");
+                prestaShopClient.ProductFactory.Update(psProduct);
+            }
+            else
+            {
+                logger.LogInformation($"Add new product id: {product.Id}, name: {product.Name}");
+                psProduct = prestaShopClient.ProductFactory.Add(psProduct);
+            }
+
+            product.WebId = (int)psProduct.id;
+
+            return psProduct;
+        }
     }
 }
